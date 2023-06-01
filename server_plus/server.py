321202995a8d75ce05abd0ee9a2e718b32fc78a9
malken21 <Malken_Marumasa@outlook.jp>
2023-06-02 04:24:38 +0900

    update

--- server_plus/server.py
+++ server_plus/server.py
@@ -1,4 +1,5 @@
 import threading
+import asyncio
 
 # プロセス関係 読み込み (統合版サーバーとの接続)
 from server_plus.process import status, main,  read_input, write_text
@@ -14,7 +15,17 @@ def run():
     input = threading.Thread(target=read_input, args=())
     input.start()
 
+    # 常時実行
+    asyncio.run(loop())
 
+
+async def loop():
+    while True:
+        await asyncio.sleep(1)
+        print("test")
+
+
+# 再起動
 def reboot():
     status["isReboot"] = False
     write_text("stop")
