399f234058f29c9f68082242a69cc8760183466d
malken21 <Malken_Marumasa@outlook.jp>
2023-06-02 18:25:27 +0900

    update

--- server_plus/util.py
+++ server_plus/util.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 
 
 # 読み込み JSON
@@ -11,3 +12,15 @@ def readJSON(path):
 def saveJSON(path, data):
     with open(path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+# 読み込み YAML
+def readYAML(path):
+    with open(path, 'r', encoding="utf-8") as file:
+        return yaml.load(file)
+
+
+# 書き込み YAML
+def saveYAML(path, data):
+    with open(path, 'w', encoding='utf-8') as file:
+        yaml.dump(data, file, indent=4, ensure_ascii=False)
